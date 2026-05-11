f9b54678b3c4c0852eb0ef97591a2047dfe06aa8
marufbytes <marufahammed215@gmail.com>
2026-05-11 21:59:00 +0600

    XM Night

--- Practice/Home Practice/Final/Form_MVC/Controller/login-controller.php
+++ Practice/Home Practice/Final/Form_MVC/Controller/login-controller.php
@@ -34,5 +34,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-include("../View/login-view.php");
+include("../iew/login-view.php");
 ?>
--- Practice/Home Practice/Final/Form_MVC/ajax/check_username.php
+++ Practice/Home Practice/Final/Form_MVC/ajax/check_username.php
@@ -7,13 +7,9 @@ $username = $_GET["username"];
 $result = checkUsername($username);
 
 if ($result->num_rows > 0) {
-
     echo json_encode(["available" => false]);
-    // JSON response sent to JavaScript: username already exists.
 
 } else {
-
     echo json_encode(["available" => true]);
-    // JSON response sent to JavaScript: username is available.
 }
 ?>
